00ccc3c3f5440c7054de59cc258d0b4b566be23b
Aastha <er.aastha27@gmail.com>
2021-02-09 15:24:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from './components/ChatFeed'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="f9985ea1-d40b-4538-b7cb-3ebf86cc9c4f"
+      userName="admin"
+      userSecret="123123"
+      renderChatFeed ={(chatProps)=> <ChatFeed  {...chatProps}/>}
+    />
   );
-}
+};
 
 export default App;
